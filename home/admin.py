3c3354b3aea256bf20e8404bf2a9f8f36c3c2d05
Camilo Mercado <mercadoarizacamilo@gmail.com>
2025-05-15 16:35:26 -0500

    Update

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models.talento_humano import DetalleAcademico, DetalleExperienciaLaboral, NivelAcademico, Rol, Tarifa, TipoDocumento, Empleado, Sede, Contrato
 from .models.carga_academica import CargaAcademica, Materia, Periodo, Programa, Semestre
-
+from .models.carga_academica.datos_adicionales import Pensum
 # Register your models here.
 admin.site.register(TipoDocumento)
 admin.site.register(Rol)
@@ -16,4 +16,5 @@ admin.site.register(Periodo)
 admin.site.register(Programa)
 admin.site.register(Semestre)
 admin.site.register(Sede)
+admin.site.register(Pensum)
 admin.site.register(Contrato)
