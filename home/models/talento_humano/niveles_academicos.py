ce38c95aadc28b017d877497199154fca572ff7f
Camilo Mercado <mercadoarizacamilo@gmail.com>
2025-05-20 09:34:32 -0500

    Cambio en Modelo "NivelAcademico"
    Se agregó el modelo "HistoricoNivelAcademico" para la información de la tarifa por hora y el año vigente en que aplica

--- home/models/talento_humano/niveles_academicos.py
+++ home/models/talento_humano/niveles_academicos.py
@@ -6,16 +6,27 @@ class NivelAcademico(models.Model):
 
     # Bachiller, Técnico Profesional, Tecnólogo, Profesional Académico, Especialización, Maestría, Doctorado, Posdoctorado
     nombre = models.CharField(max_length=255, unique=True)
-    descripcion = models.TextField(null=True, blank=True)
-    tarifa_base_por_hora = models.DecimalField(max_digits=10, decimal_places=2)
-    vigencia = models.PositiveIntegerField()
 
     class Meta:
         db_table = 'niveles_academicos'
         verbose_name = 'Nivel Académico'
         verbose_name_plural = 'Niveles Académicos'
-        # Evita duplicados para el mismo nivel académico y año
-        unique_together = ('nombre', 'vigencia')
 
     def __str__(self):
         return f"{self.nombre}"
+
+
+class NivelAcademicoHistorico(models.Model):
+    id = models.AutoField(primary_key=True)
+    fk_nivel_academico = models.ForeignKey(NivelAcademico, on_delete=models.CASCADE)
+    tarifa_base_por_hora = models.DecimalField(max_digits=10, decimal_places=2)
+    año_vigencia = models.PositiveIntegerField()
+
+    class Meta:
+        db_table = 'niveles_academicos_historico'
+        verbose_name = 'Nivel Académico Histórico'
+        verbose_name_plural = 'Niveles Académicos Históricos'
+        unique_together = ('fk_nivel_academico', 'año_vigencia')
+
+    def __str__(self):
+        return f"{self.fk_nivel_academico.nombre} - ${self.tarifa_base_por_hora} - {self.vigencia}"
