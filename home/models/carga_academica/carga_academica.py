ec0ad73da790f0f0fc0840cfce620868df710cec
Camilo Mercado <mercadoarizacamilo@gmail.com>
2025-05-23 10:47:05 -0500

    Update Models

--- home/models/carga_academica/carga_academica.py
+++ home/models/carga_academica/carga_academica.py
@@ -11,15 +11,17 @@ class CargaAcademica(models.Model):
     fk_semestre = models.ForeignKey(Semestre, on_delete=models.CASCADE, verbose_name="Semestre")
     fk_materia = models.ForeignKey(Materia, on_delete=models.CASCADE, verbose_name="Materia")
     fk_docente_asignado = models.ForeignKey(Empleado, on_delete=models.CASCADE, verbose_name="Docente Asignado")
-    horas_semanales = models.CharField("Horas Semanales", max_length=255, null=True, blank=True)
-    total_horas = models.CharField("Total Horas", max_length=255, null=True, blank=True)
+    horas_semanales = models.IntegerField("Horas Semanales", null=True, blank=True)
+    total_horas = models.IntegerField("Total Horas", null=True, blank=True)
     valor_a_pagar = models.IntegerField("Valor a Pagar", null=True, blank=True)
     materia_compartida = models.BooleanField("Materia Compartida", default=False, null=True, blank=True)
 
     # Creación de carga académica
-    creado_por = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='carga_academica_creado_por', verbose_name="Creado Por", on_delete=models.SET_NULL, null=True, blank=True)
+    fk_creado_por = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='carga_academica_creado_por', verbose_name="Creado Por", on_delete=models.SET_NULL, null=True, blank=True)
     fecha_creacion = models.DateTimeField("Fecha Creación", auto_now_add=True, null=True, blank=True)
-    modificado_por = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='carga_academica_modificado_por', verbose_name="Modificado Por", on_delete=models.SET_NULL, null=True, blank=True)
+
+    # Modificación de la carga académica
+    fk_modificado_por = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='carga_academica_modificado_por', verbose_name="Modificado Por", on_delete=models.SET_NULL, null=True, blank=True)
     fecha_modificacion = models.DateTimeField("Fecha Modificación", auto_now=True, null=True, blank=True)
 
     # Aprobación vicerectoria
@@ -59,3 +61,20 @@ class MateriaCompartida(models.Model):
 
     def __str__(self):
         return f"{self.fk_carga_academica} - {self.fk_docente_asignado} - Horas: {self.horas_semanales} - Valor a Pagar: ${self.valor_a_pagar}"
+
+
+class FuncionesSustantivas(models.Model):
+    fk_carga_academica = models.ForeignKey(CargaAcademica, on_delete=models.CASCADE, verbose_name="Carga Académica")
+    horas_administrativas = models.IntegerField("Horas por Administrativas", null=True, blank=True)
+    horas_docencia = models.IntegerField("Horas por Docencia", null=True, blank=True)
+    horas_investigacion = models.IntegerField("Horas por Investigación", null=True, blank=True)
+    horas_otras_actividades = models.IntegerField("Horas por Otras Actividades", null=True, blank=True)
+    horas_proyeccion_extension = models.IntegerField("Horas por Proyección y Extensión", null=True, blank=True)
+
+    class Meta:
+        db_table = "funciones_sustantivas"
+        verbose_name = "FuncionesSustantivas"
+        verbose_name_plural = "FuncionesSustantivass"
+
+    def __str__(self):
+        return self.name
